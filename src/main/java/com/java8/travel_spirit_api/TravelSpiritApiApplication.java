b390a9a0dae12b007c6a681f35af0894ebd0ae0d
dhstambolliu <stambolliudhimiter@gmail.com>
2022-05-15 16:26:28 +0200

    backend empty project

---
+++ src/main/java/com/java8/travel_spirit_api/TravelSpiritApiApplication.java
@@ -0,0 +1,13 @@
+package com.java8.travel_spirit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSpiritApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSpiritApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java8/travel_spirit_api/TravelSpiritApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java8.travel_spirit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSpiritApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
